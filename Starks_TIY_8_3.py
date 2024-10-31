4e19e319863706f6b8dd645b35cfd5e45041818b
Julian <julian-jordan.starks@fwcsstudents.org>
2024-10-31 12:52:29 -0400

    Delete Starks_TIY_8_3.py

--- Starks_TIY_8_3.py
+++
@@ -1,8 +0,0 @@
-def make_shirt(shirt_size, shirt_message):
-    """"""
-    print(f"\nThe shirt will be a {shirt_size}.")
-    print(f'The message on the shirt says "{shirt_message}"')
-
-make_shirt('medium', 'awesome sauce')
-
-make_shirt(shirt_message='awesome sauce', shirt_size='medium')
